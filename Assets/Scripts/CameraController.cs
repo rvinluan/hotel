444b561dfc7df27cbf7741700b8e0a1a28386c0d
Robert Vinluan <robert.vinluan@nytimes.com>
2017-02-18 16:25:31 -0500

    first commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+ 
+public class CameraController : MonoBehaviour {
+    public float moveSpeed = 1f;
+    private Vector3 dragOrigin;
+
+    void Update() {
+      if (Input.GetMouseButtonDown(0)) {
+          dragOrigin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+          return;
+      }
+      if (!Input.GetMouseButton(0)) {
+          return;
+      }
+
+      Vector3 delta = dragOrigin - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      transform.Translate(delta * moveSpeed, Space.World);
+    }
+}
