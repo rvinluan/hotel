ad9e04e1c917c3e67739e18e17e19b60cb34cafb
kwistens <kristen.leach@gmail.com>
2017-06-04 17:47:00 -0400

    added start/end screens, variable routing, fixed bug stuck between rooms

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -36,6 +36,10 @@ public struct Destination {
   public Room room;
   public Activity activity;
   public int time;
+	//where you're aiming to get to in the room
+	// scalar from range 0 to 1 of room, relative to leftmost room position
+	//used as multiplier of width
+	public float roomTarget;
 }
 
 public class Map : MonoBehaviour {
@@ -66,13 +70,19 @@ public class Map : MonoBehaviour {
   public List<Room>[] floors;
   public List<Room> allRooms;
   private Person theOne = null;
+
+  //only plays from one stairwell, bottom
+  //only need reference to single stairwell for audio purposes
   private GameObject stairwell = null; 
 
   public void initializeMap () {
     float highest = 0;
     float leftmost = 0;
+	// i is  vertical for each floor in array
+	// j is horizontal each room on floor
     for (int i = currentMap.Length - 1; i >= 0; i--) {
-      int h = currentMap.Length - 1 - i;
+      // hs is floor height from bottom of building. floor 0 is first
+	  int h = currentMap.Length - 1 - i;
       floors[h] = new List<Room>();
       string[] roomsOnFloor = currentMap[i].Split("|"[0]);
       for (int j = 0; j < roomsOnFloor.Length; j++) {
@@ -81,30 +91,43 @@ public class Map : MonoBehaviour {
           //add stairs
           Vector3 stairpos = new Vector3(leftmost, highest, 5);
           GameObject stairpfab = Resources.Load("Room Prefabs/StairsLeft") as GameObject;
+		  //quaternion.identity is default no rotation
           GameObject stairgo = Instantiate(stairpfab, stairpos, Quaternion.identity) as GameObject;
           leftmost += stairgo.GetComponent<SpriteRenderer>().bounds.size.x;
           if(i == currentMap.Length - 1) {
+			//set stairwell reference to bottom stair game object
             stairwell = stairgo;
           }
+		  //destroy stairs if on top floor
           if (i == 0) {
             Object.Destroy(stairgo);
           }
+		  //for two rooms high, make extra set of stairs
           if(roomsOnFloor[j] == "Lobby" || roomsOnFloor[j] == "Parking" || roomsOnFloor[j] == "Pool") {
             stairpos.y += stairgo.GetComponent<SpriteRenderer>().bounds.size.y;
             GameObject stairgo2 = Instantiate(stairpfab, stairpos, Quaternion.identity) as GameObject;
           }
         }
+		// next to last room drawn
         Vector3 pos = new Vector3(leftmost, highest, 5);
         GameObject pfab = Resources.Load("Room Prefabs/"+roomsOnFloor[j]) as GameObject;
         GameObject go = Instantiate(pfab, pos, Quaternion.identity) as GameObject;
         Room rm = go.GetComponent<Room>();
-        rm.roomID = "Test";
+      	// not yet used
+		//rm.roomID = "Test";
+
+		// give string and get back roomtype
+		// guarantees type strictness
         rm.type = (RoomType)System.Enum.Parse(typeof(RoomType), roomsOnFloor[j]);
+		//easily get size of sprite and placement
+		//bounds docs for more info
         Bounds sp = rm.gameObject.GetComponent<SpriteRenderer>().bounds;
         rm.width = sp.size.x;
         rm.height = sp.size.y;
         rm.floor = h;
+		// add to the array by h, so floors[0] is bottom floor
         floors[h].Add(rm);
+		// track all room for audio
         allRooms.Add(rm);
         leftmost += rm.width;
         if (j == roomsOnFloor.Length - 1) {
@@ -140,25 +163,38 @@ public class Map : MonoBehaviour {
     if(theOne == null) {
       theOne = GameObject.Find("PersonManager").GetComponent<PersonManager>().theOne;
     }
+	//turn off all audio 
     for(int i = 0; i < allRooms.Count; i++) {
       AudioSource audio = allRooms[i].GetComponent<AudioSource>();
       audio.Pause();
     }
+	//only turn on audio that is in the one's room
     for(int j = 0; j < floors[theOne.currentFloor].Count; j++) {
       Room rm = floors[theOne.currentFloor][j];
+	  //audio source assigned to room in prefab
+	  //change to attach multiple to prefab and then choose random in room script
       AudioSource audio = rm.GetComponent<AudioSource>();
+	  //get center of room that the ONE is in
       Vector3 roomCenter = new Vector3(rm.transform.position.x + rm.width/2, rm.transform.position.y, 0);
-      float dist = Vector3.Distance(roomCenter, theOne.transform.position);
-      if(dist < 11) {
+      //how far the one is from center of room
+	  float dist = Vector3.Distance(roomCenter, theOne.transform.position);
+      
+	  //11 is radius to audio
+	  if(dist < 11) {
         if(!audio.isPlaying) {
           audio.Play(); 
         }
         audio.volume = Mathf.Lerp(1, 0.01f, dist/11);
       } else {
+		//stop audio if too far
         audio.Pause();
       }
     }
     //always play the audio from the room you're in;
+	//if not in a room, then you're in stairwell
+	// it's possible for large rooms to be bigger than 11 radius, which means no sound will play
+	// even if you're in the room
+	//so we do one extra check 
     if(theOne.findRoomPlayerIsIn() == null) {
       if(!stairwell.GetComponent<AudioSource>().isPlaying) {
         stairwell.GetComponent<AudioSource>().Play();
@@ -167,6 +203,7 @@ public class Map : MonoBehaviour {
       AudioSource a = theOne.findRoomPlayerIsIn().GetComponent<AudioSource>();
       if(!a.isPlaying) {
         a.Play();
+		//better to hear full volume of sound than to lerp
         a.volume = 1;
       }
       stairwell.GetComponent<AudioSource>().Pause();
--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -27,38 +27,51 @@ public enum SkinType {
 }
 
 public class Person : MonoBehaviour {
-  public float walkingSpeed = 0.01f;
+  
   private List<Destination> route = new List<Destination>();
   public int currentStop;
   public int currentFloor;
   public int timer;
   public Activity currentlyDoing;
-  private Map map;
+  private Map mapRef;
   private SkeletonAnimation skeletonAnimation;
   public Spine.Skeleton skeleton;
   private bool facingLeft = true;
   public bool isTheOne = false;
+  private float walkingSpeed;
 
   Destination generateRandomDestination() {
-    int r = Random.Range(0, map.floors.Length);
-    List<Room> randomFloor = map.floors[r];
+    int r = Random.Range(0, mapRef.floors.Length);
+    List<Room> randomFloor = mapRef.floors[r];
     Room randomRoom = randomFloor[ Random.Range(0, randomFloor.Count) ];
     Destination d = new Destination();
     d.room = randomRoom;
     d.activity = Activity.Nothing;
     d.time = 300;
+	d.roomTarget = Random.Range (0.2f, .8f);
     return d;
   }
 
 	// Use this for initialization
 	void Start () {
+	//randomize character's walking speed
+	walkingSpeed = Random.Range (0.01f, 0.03f);
+
+	//assign character skin from skinType
     skeletonAnimation = GetComponent<SkeletonAnimation>();
     skeleton = skeletonAnimation.Skeleton;
     skeleton.SetSkin( ((SkinType)Random.Range(0, 20)).ToString() );
-    map = GameObject.Find("Map").GetComponent<Map>();
-    currentStop = -1;
+    //get reference to map
+	mapRef = GameObject.Find("Map").GetComponent<Map>();
+    // this stores where in current route-- when you start not yet at first stop, at spawn position
+	currentStop = -1;
+	// determines sprite animation, when you get to room then do animation assigned
+	// but first start in transit
+	// could start by idling
+	
     currentlyDoing = Activity.Transit;
     timer = 0;
+	// create 5 destinations in route
     for(int i = 0; i <= 5; i++) {
       route.Add(generateRandomDestination());
     }
@@ -78,9 +91,9 @@ public class Person : MonoBehaviour {
     //if you're in transit
     if (currentlyDoing == Activity.Transit) {
       int nextStop = currentStop < route.Count - 1 ? currentStop + 1 : 0;
-      Room destination = route[nextStop].room;
+      Room destinationRoom = route[nextStop].room;
       //if you've reached your destination
-      if (reachedDestination(destination)) {
+	  if (reachedDestination(route[nextStop])) {
         currentStop++;
         if(currentStop >= route.Count) {
           currentStop = 0;
@@ -89,14 +102,14 @@ public class Person : MonoBehaviour {
         //start doing your thing!
         skeletonAnimation.AnimationName = "idle";
       } else { //if you're not at your destination yet
-        if (currentFloor != destination.floor) {
+        if (currentFloor != destinationRoom.floor) {
           if (currentlyOnStairs()) {
-            if (doneWithStairs(destination)) {
-              currentFloor = destination.floor;
+            if (doneWithStairs(destinationRoom)) {
+              currentFloor = destinationRoom.floor;
               return;
             }
             //running on stairs because stairs are boring
-            if (currentFloor > destination.floor) {
+            if (currentFloor > destinationRoom.floor) {
               //going down stairs
               transform.Translate(0, -walkingSpeed*5, 0);
             } else {
@@ -120,9 +133,13 @@ public class Person : MonoBehaviour {
           }
         } else {
           //walk towards destination
-          Vector3 roomCenter = destination.transform.position;
-          roomCenter.x += destination.width*0.5f;
-          Vector3 delta = roomCenter - transform.position;
+		  //pick random point in bounds of room
+		  //
+		  //gets the leftmost position of destination room
+          Vector3 targetLocation = destinationRoom.transform.position;
+		  //adds offset for where in that room to go toward
+		  targetLocation.x += route [nextStop].roomTarget * destinationRoom.width;
+          Vector3 delta = targetLocation - transform.position;
           delta = Vector3.ClampMagnitude(delta, walkingSpeed);
           transform.Translate(delta.x, 0, 0);
           facingLeft = delta.x < 0;
@@ -143,8 +160,8 @@ public class Person : MonoBehaviour {
   }
 
   public Room findRoomPlayerIsIn() {
-    for(int j = 0; j < map.floors[currentFloor].Count; j++) {
-      Room r = map.floors[currentFloor][j];
+    for(int j = 0; j < mapRef.floors[currentFloor].Count; j++) {
+      Room r = mapRef.floors[currentFloor][j];
       float minX = r.transform.position.x;
       float maxX = minX + r.width;
       if( transform.position.x >= minX && transform.position.x <= maxX ) {
@@ -158,9 +175,17 @@ public class Person : MonoBehaviour {
     return transform.position.x < 3.75/2 || transform.position.x > (45.00 - (3.75/2));
   }
 
-  public bool reachedDestination(Room destination) {
-    float center = (destination.transform.position.x + destination.width*0.5f);
-    return findRoomPlayerIsIn() == destination && transform.position.x >= center - 0.2;
+  public bool reachedDestination(Destination destination) {
+		
+	Vector3 target = destination.room.transform.position;
+	target.x += destination.roomTarget * destination.room.width;
+	target.z = 0;
+	Vector3 delta = target - transform.position;
+	delta.z = 0;
+	Debug.DrawLine (transform.position, target);
+	//Debug.Log ("Delta: "+delta);
+	//Debug.Log ("Magnitude: "+delta.magnitude);
+    return delta.magnitude <= 0.5f;
   }
 
   public bool doneWithStairs(Room destination) {
